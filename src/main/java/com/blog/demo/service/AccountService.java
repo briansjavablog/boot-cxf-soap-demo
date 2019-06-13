ca4c455676db0f6f37208f49dc03c8ea20aea9cd
unknown <hannawaybrian@googlemail.com>
2019-06-13 17:36:49 +0100

    removed annotations that aren't needed and removed service class to simplify things a bit

--- src/main/java/com/blog/demo/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.blog.demo.service;
-
-import com.blog.demo.webservices.accountservice.AccountDetailsRequest;
-import com.blog.demo.webservices.accountservice.AccountDetailsResponse;
-
-public interface AccountService {
-
-	AccountDetailsResponse getAccountDetails(AccountDetailsRequest parameters);
-}
--- src/main/java/com/blog/demo/service/AccountServiceEndpoint.java
+++ src/main/java/com/blog/demo/service/AccountServiceEndpoint.java
@@ -2,29 +2,31 @@ package com.blog.demo.service;
 
 import javax.jws.WebService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.blog.demo.webservices.accountservice.Account;
 import com.blog.demo.webservices.accountservice.AccountDetailsRequest;
 import com.blog.demo.webservices.accountservice.AccountDetailsResponse;
+import com.blog.demo.webservices.accountservice.EnumAccountStatus;
+import com.blog.demo.webservices.accountservice.ObjectFactory;
 
 @Service
-@WebService(portName = "Accounts", serviceName = "Accounts", 
-			endpointInterface = "com.briansdevblog.accounts.AccountService", 
-			targetNamespace = "http://www.briansjavablog.com/AccountService/")
-public class AccountServiceEndpoint implements AccountService {
-
-	private AccountService accountService;
-
-	@Autowired	
-	public AccountServiceEndpoint(@Qualifier("accountServiceImpl") AccountService accountService) {
-		this.accountService = accountService;
-	}
-
+public class AccountServiceEndpoint implements com.briansdevblog.accounts.AccountService {
+	
 	@Override
 	public AccountDetailsResponse getAccountDetails(AccountDetailsRequest parameters) {
 
-		return accountService.getAccountDetails(parameters);
+		ObjectFactory factory = new ObjectFactory();
+		AccountDetailsResponse response = factory.createAccountDetailsResponse();
+		
+		Account account = factory.createAccount();
+		account.setAccountNumber("12345");
+		account.setAccountStatus(EnumAccountStatus.ACTIVE);
+		account.setAccountName("Joe Bloggs");
+		account.setAccountBalance(3400);
+		
+		response.setAccountDetails(account);		
+		return response;
 	}
+	
 }
